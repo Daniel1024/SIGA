45e9161e560dd7a541cb55f4b010e59ea7319b1e
Daniel Lopez <tierra_santa_9@hotmail.com>
2016-04-08 17:12:16 -0500

    Primera vista

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 $(document).ready(function (){
-   alert('Hola Mundo');
+   $('#toggle-menu').click(function(ev) {
+      ev.preventDefault();
+      $(this).next().slideToggle();
+   });
+   var $menu = $('.menu').find('li');
+   $menu.on('click', 'a', function (ev){
+      ev.preventDefault();
+      var $toggle = $('#toggle-menu')
+      if ($toggle.css('display') == 'block') {
+         $toggle.next().slideToggle();
+      }
+      $('html, body').animate({
+         scrollTop: $($(this).attr('href')).offset().top
+      }, 1000);
+   });
+   $('.owl-carousel').owlCarousel({
+      loop:true,
+      //nav:true,
+      dots:true,
+      items:1,
+      center:true,
+      //autoplay:true,
+      autoplayHoverPause:true,
+      autoplayTimeout:10000,
+      smartSpeed:500
+      //navText:['<i class="fa fa-caret-left fa-5x"></i>', '<i class="fa fa-caret-right fa-5x"></i>']
+   });
 });
