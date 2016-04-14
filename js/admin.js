c39322dbc03d472498c7e72f1c74c09ffe59144a
Daniel Lopez <tierra_santa_9@hotmail.com>
2016-04-14 12:35:30 -0500

    Agregando efectos del menu

--- js/admin.js
+++ js/admin.js
@@ -1,2 +1,29 @@
 $(document).ready(function (){
+   var $URLactual = window.location.pathname;
+   var $menu = $('.sidebar').find('ul');
+   $('.sidebar').find('a').each(function(index) {
+      //console.log( index + ": " + $(this).attr('href') );
+      var $text = $(this).attr('href');
+      if ($text[0] !== '/') {
+         $text = '/' + $text;
+      }
+      if ($URLactual === $text) {
+         $(this).parent().addClass('active');
+         //console.log($(this).parent().parent().parent().hasClass('parent'));
+         if (!$(this).parent().parent().parent().hasClass('parent')) {
+            $menu.find('li.parent>ul').slideToggle();
+         }
+         return false;
+      }
+   });
+   //console.log($URLactual);
+   /*Codigo para el dropdown del menu*/
+   $menu.on('click', 'li.parent>a', function (ev){
+      ev.preventDefault();
+      var $elemento = $(this);
+      $elemento.find('>i').toggleClass('fa-chevron-circle-up').toggleClass('fa-chevron-circle-down');
+      $(this).next().slideToggle();
+      //console.log($(this))
+   });
+   /*Codigo para el dropdown del menu*/
 });
